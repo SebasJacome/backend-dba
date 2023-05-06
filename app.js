b8fd73acae65142fdd9e38737755d24082c3f960
SebasJacome <0237105@up.mx>
2023-05-06 13:10:50 -0600

    DB connection, added endpoint for countrycode in order to add and show

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const {PrismaClient} = require ('@prisma/client')
+const app = express();
+const prisma = new PrismaClient()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// Crear un registro
+// app.post('/post', async(req, res) => {
+//     const {title, content} = req.body
+//     const result => await prisma.post.create({
+    
+//         data: {
+//             title, content
+//         }
+//     })
+//     res.json(result)
+// })
+
+// Mostrar todos los Registros
+app.get('/posts', async(req, res) => {
+    const posts = await prisma.countrycode.findMany()
+    res.json(posts)
+})
+
+
+app.listen(3000, () =>
+    console.log('Server ready at: http://localhost:3000')
+)
